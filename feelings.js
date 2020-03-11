41a84be7aaf68f36ec5ddd737e6459f96439246a
Joakim Nystrom <jnystromdesign@gmail.com>
2020-03-11 08:30:13 +0100

    Adding file for feelings

---
+++ feelings.js
@@ -0,0 +1,232 @@
+const feelings = {
+  fear: {
+    subs: {
+      scared: {
+        subs: {
+          helpless: {},
+          frightened: {}
+        }
+      },
+      terror: {
+        subs: {
+          hysterical: {},
+          panic: {}
+        }
+      },
+      insecure: {
+        subs: {
+          inadequate: {},
+          inferrior: {}
+        }
+      },
+      nervous: {
+        subs: {
+          anxious: {},
+          worried: {}
+        }
+      },
+      horror: {
+        subs: {
+          helpless: {},
+          frightened: {}
+        }
+      }
+    }
+  },
+  anger: {
+    subs: {
+      rage: {
+        subs: {
+          hostile: {},
+          hate: {}
+        }
+      },
+      exasperated: {
+        subs: {
+          frustrated: {},
+          agitated: {}
+        }
+      },
+      irritable: {
+        subs: {
+          aggrevated: {},
+          annoyed: {}
+        }
+      },
+      envy: {
+        subs: {
+          resentful: {},
+          jealous: {}
+        }
+      },
+      disgust: {
+        subs: {
+          contempt: {},
+          revolted: {}
+        }
+      }
+    }
+  },
+  sadness: {
+    subs: {
+      suffering: {
+        subs: {
+          agony: {},
+          hurt: {}
+        }
+      },
+      sadness: {
+        subs: {
+          depressed: {},
+          sorrow: {}
+        }
+      },
+      dissapointed: {
+        subs: {
+          frustrated: {},
+          agitated: {}
+        }
+      },
+      shameful: {
+        subs: {
+          regretful: {},
+          guilty: {}
+        }
+      },
+      neglected: {
+        subs: {
+          isolated: {},
+          lonely: {}
+        }
+      },
+      despair: {
+        subs: {
+          grief: {},
+          powerless: {}
+        }
+      }
+    }
+  },
+
+  love: {
+    subs: {
+      peaceful: {
+        subs: {
+          satisfied: {},
+          relieved: {}
+        }
+      },
+      tenderness: {
+        subs: {
+          compassionate: {},
+          caring: {}
+        }
+      },
+      desire: {
+        subs: {
+          infatuation: {},
+          passion: {}
+        }
+      },
+      longing: {
+        subs: {
+          attracted: {},
+          senitmental: {}
+        }
+      },
+      affectionate: {
+        subs: {
+          fondness: {},
+          romantic: {}
+        }
+      }
+    }
+  },
+  joy: {
+    subs: {
+      content: {
+        subs: {
+          pleased: {},
+          satisfied: {}
+        }
+      },
+      happy: {
+        subs: {
+          amused: {},
+          delighted: {}
+        }
+      },
+      cheerful: {
+        subs: {
+          jovial: {},
+          blissful: {}
+        }
+      },
+      proud: {
+        subs: {
+          triumphant: {},
+          illustrious: {}
+        }
+      },
+      optimistic: {
+        subs: {
+          hopeful: {},
+          eager: {}
+        }
+      },
+      entusiastic: {
+        subs: {
+          zeal: {},
+          excited: {}
+        }
+      },
+      elation: {
+        subs: {
+          jubilation: {},
+          euphoric: {}
+        }
+      },
+      enthralled: {
+        subs: {
+          rapture: {},
+          enchanted: {}
+        }
+      }
+    }
+  },
+  surprise: {
+    subs: {
+      stunned: {
+        subs: {
+          shocked: {},
+          dismayed: {}
+        }
+      },
+      confused: {
+        subs: {
+          disillusion: {},
+          perplexed: {}
+        }
+      },
+      amazed: {
+        subs: {
+          astonished: {},
+          awe_struck: {}
+        }
+      },
+      overcome: {
+        subs: {
+          speachless: {},
+          astounded: {}
+        }
+      },
+      moved: {
+        subs: {
+          stimulated: {},
+          touched: {}
+        }
+      }
+    }
+  }
+};
+export default feelings;
